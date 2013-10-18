78c624bebb8d96660f7c2364b927ceda67caf4fd
WerthersEchte <arabali@gmx.de>
2013-10-18 19:31:28 +0200

    &$%&$%&")((/?

--- bot_mrlib/src/mrlib/core/PositionLib.java
+++ bot_mrlib/src/mrlib/core/PositionLib.java
@@ -30,93 +30,59 @@ public class PositionLib {
      * @return einen ReferencePoint mit den Koordinaten des Mittelpunkts der Parameter
      */
     public static ReferencePoint getMiddleOfTwoReferencePoints( ReferencePoint aFirstReferencePoint, ReferencePoint aSecondReferencePoint ){
-        
-        double vMaxDegree, vMinDegree, vResultDegree;
-        
-        if( aFirstReferencePoint.getAngleToPoint() > aSecondReferencePoint.getAngleToPoint() ){
-            vMaxDegree = aFirstReferencePoint.getAngleToPoint() + 180.0;
-            vMinDegree = aSecondReferencePoint.getAngleToPoint() + 180.0;
-        }
-        else{
-            vMaxDegree = aSecondReferencePoint.getAngleToPoint() + 180.0;
-            vMinDegree = aFirstReferencePoint.getAngleToPoint() + 180.0;
-        }
-        
-        vResultDegree = vMaxDegree - vMinDegree;
-        
-        if( vResultDegree > 180 ) {
-            
-            vResultDegree = 360 - vResultDegree;
-            
-        }
-
-        if(vMaxDegree > 180 && vMinDegree < 90){
-            
-            vResultDegree = ( vMaxDegree + vResultDegree / 2 ) - 180;
-            if ( vResultDegree > 180 ) {
-                
-                vResultDegree = vResultDegree - 360;
-                
-            }
-              
-        } else {
-           
-            vResultDegree = ( vMinDegree + vResultDegree / 2 ) - 180;
-            
-        }
-        
-        return new ReferencePoint( 10.0 , vResultDegree );
-        
+       
+        return null;
+     
     }
     
     
-	/**
-	 * Returns the middle of a goal
-	 * @param aWorldData
-	 * @param aTeam Team color of the goal
-	 * @return ReferencePoint
-	 */
-	public static ReferencePoint getMiddleOfGoal( RawWorldData aWorldData, Teams aTeam ){		
-		ReferencePoint rGoalMiddle = null;
-		ReferencePoint vGoalTop = null;
-		ReferencePoint vGoalBottom = null;
-		ReferencePoint vGoalMax = null;
-		ReferencePoint vGoalMin = null;
-		
-		// get goal
-		if( aTeam == Teams.Blue ) {
-			vGoalTop = aWorldData.getBlueGoalCornerTop();
-			vGoalBottom = aWorldData.getBlueGoalCornerBottom();
-		}
-		else{
-			vGoalTop = aWorldData.getYellowGoalCornerTop();
-			vGoalBottom = aWorldData.getYellowGoalCornerBottom();
-		}
-		
-		// get min und max angle
-		if( Math.abs(vGoalTop.getAngleToPoint())
-				> Math.abs(vGoalBottom.getAngleToPoint()) ){
-			vGoalMax = vGoalTop;
-			vGoalMin = vGoalBottom;
-		}
-		else{
-			vGoalMax = vGoalBottom;
-			vGoalMin = vGoalTop;
-		}
-		
-		// TODO: Calculate distance to goal
-		
-		rGoalMiddle.setAngelToPoint( vGoalMin.getAngleToPoint()
-				+ (vGoalMax.getAngleToPoint() - vGoalMin.getAngleToPoint()) );		
-		return rGoalMiddle;
-	}
-	
-	/**
-	 * Get's the field position that's furthest away from ball
-	 * @param aWorldData
-	 * @return ReferencePoint
-	 */
-	public static ReferencePoint getBestPointAwayFromBall( RawWorldData aWorldData ) {        
+    /**
+     * Returns the middle of a goal
+     * @param aWorldData
+     * @param aTeam Team color of the goal
+     * @return ReferencePoint
+     */
+    public static ReferencePoint getMiddleOfGoal( RawWorldData aWorldData, Teams aTeam ){       
+        ReferencePoint rGoalMiddle = new ReferencePoint( 0.0 , 0.0 );
+        ReferencePoint vGoalTop = null;
+        ReferencePoint vGoalBottom = null;
+        ReferencePoint vGoalMax = null;
+        ReferencePoint vGoalMin = null;
+        
+        // get goal
+        if( aTeam == Teams.Blue ) {
+            vGoalTop = aWorldData.getBlueGoalCornerTop();
+            vGoalBottom = aWorldData.getBlueGoalCornerBottom();
+        }
+        else{
+            vGoalTop = aWorldData.getYellowGoalCornerTop();
+            vGoalBottom = aWorldData.getYellowGoalCornerBottom();
+        }
+        
+        // get min und max angle
+        if( Math.abs(vGoalTop.getAngleToPoint())
+                > Math.abs(vGoalBottom.getAngleToPoint()) ){
+            vGoalMax = vGoalTop;
+            vGoalMin = vGoalBottom;
+        }
+        else{
+            vGoalMax = vGoalBottom;
+            vGoalMin = vGoalTop;
+        }
+        
+        // TODO: Calculate distance to goal
+        
+        rGoalMiddle.setAngelToPoint( vGoalMin.getAngleToPoint()
+                + (vGoalMax.getAngleToPoint() - vGoalMin.getAngleToPoint()) );      
+        return rGoalMiddle;
+    }
+    
+    /**
+     * Get's the field position that's furthest away from ball
+     * @param aWorldData
+     * @return ReferencePoint
+     */
+    public static ReferencePoint getBestPointAwayFromBall( RawWorldData aWorldData ) {        
         ReferencePoint vBestPoint = null;
         double vBestAngle = 0;
         
@@ -153,5 +119,5 @@ public class PositionLib {
         
         return vBestPoint;        
     }
-	
+    
 }
--- bot_mrlib/tests/core/PostionLibrary.java
+++ bot_mrlib/tests/core/PostionLibrary.java
@@ -39,6 +39,14 @@ public class PostionLibrary {
         assertEquals(" Der Winkel muss -168 Grad betragen.", -168, vResultingPoint.getAngleToPoint(), 0.1);
         assertEquals(" Die Distanz muss 10 betragen.", 10, vResultingPoint.getDistanceToPoint(), 0.1 );
 
+        vFirstPoint = new ReferencePoint( 55.0 , 140.0 );       
+        vSecondPoint = new ReferencePoint( 10.0 , -30.0 );;
+        
+        vResultingPoint = mrlib.core.PositionLib.getMiddleOfTwoReferencePoints( vFirstPoint, vSecondPoint );
+
+        assertEquals(" Der Winkel muss 90 Grad betragen.", 90, vResultingPoint.getAngleToPoint(), 0.1);
+        assertEquals(" Die Distanz muss 10 betragen.", 10, vResultingPoint.getDistanceToPoint(), 0.1 );
+
         vFirstPoint = new ReferencePoint( 55.0 , 90.0 );       
         vSecondPoint = new ReferencePoint( 10.0 , -90.0 );;
         
