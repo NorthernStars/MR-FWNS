3b35aa6f30a02d7ccf02f49aa97adb2fbdfad99a
Hannes Eilers <hanneseilers@gmx.de>
2014-04-05 14:54:55 +0200

    Added botloader

---
+++ bot_remotecontrol/src/core/BotLoader.java
@@ -0,0 +1,27 @@
+package core;
+
+import essentials.core.BotInformation;
+
+public class BotLoader implements Runnable  {
+
+	private BotInformation mBot;
+	
+	public BotLoader(BotInformation aBot) {
+		mBot = aBot;
+	}
+	
+	public boolean startBot(){
+		if( mBot != null ){
+			
+		}
+		
+		return false;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
