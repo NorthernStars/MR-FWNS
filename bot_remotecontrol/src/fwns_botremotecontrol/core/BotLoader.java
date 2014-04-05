4034c725834e4763b6e685106094a426e64dfb6f
Hannes Eilers <hanneseilers@gmx.de>
2014-04-05 16:02:26 +0200

    Updated

--- bot_remotecontrol/src/fwns_botremotecontrol/core/BotLoader.java
+++ bot_remotecontrol/src/fwns_botremotecontrol/core/BotLoader.java
@@ -1,27 +1,85 @@
 package fwns_botremotecontrol.core;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.Level;
+
 import essentials.core.BotInformation;
 
-public class BotLoader implements Runnable  {
+public class BotLoader implements Runnable {
 
-	private BotInformation mBot;
-	
+	private BotInformation mBotinformation;
+	private Process mProcess;
+	private BufferedReader mReader;
+	private boolean active = false;
+
 	public BotLoader(BotInformation aBot) {
-		mBot = aBot;
+		mBotinformation = aBot;
 	}
-	
-	public boolean startBot(){
-		if( mBot != null ){
-			
+
+	public boolean startBot() {
+		try {
+
+			if (mBotinformation != null) {
+
+				
+				List<String> argList = new ArrayList<String>();
+				argList.add("test.bat");
+				argList.add("-cp");
+				argList.add("\"libraries/*:bot_mr.jar\"");
+				argList.add("core.Main");
+				
+				// create processbuilder
+				ProcessBuilder processBuilder = new ProcessBuilder(argList);
+				processBuilder.redirectErrorStream(true);
+				processBuilder.directory(new File("../MixedRealityBot"));
+				
+				// start process
+				mProcess = processBuilder.start();
+				
+				// get process inputstream reader
+				mReader = new BufferedReader(new InputStreamReader(mProcess.getInputStream()));
+
+				active = true;
+				(new Thread(this)).start();
+				
+				Thread.sleep(5000);
+				active = false;
+				mProcess.destroy();
+				mProcess = null;
+				
+			}
+
+		} catch (Exception vException) {
+			Core.getLogger().error(
+					"Error loading FWNS core "
+							+ vException.getLocalizedMessage());
+			Core.getLogger().catching(Level.ERROR, vException);
 		}
-		
+
 		return false;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		try{
+			
+			while( active && mReader != null ){
+				String line = mReader.readLine();
+				if( line != null ){
+					System.out.println( "# " + line );
+				}
+			}
+			
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+
 	}
-	
+
 }
--- bot_remotecontrol/src/fwns_botremotecontrol/core/Main.java
+++ bot_remotecontrol/src/fwns_botremotecontrol/core/Main.java
@@ -2,10 +2,15 @@ package fwns_botremotecontrol.core;
 
 import java.lang.management.ManagementFactory;
 
+import essentials.core.BotInformation;
+
 public class Main {
     
     public static void main( String[] aCommandline ) {
         
+    	BotLoader botloader = new BotLoader(new BotInformation());
+    	Core.getLogger().info("loaded bot: " + botloader.startBot());
+    	
         System.setProperty("RemoteControl", ManagementFactory.getRuntimeMXBean().getName() + "" );
         Core.getLogger().info("Starting RemoteControl(" + ManagementFactory.getRuntimeMXBean().getName() + ")" );
         
