3a4a5bab5c72083f141c69284d3115b12fdf6d75
hannes eilers <hannes@sallysue-thinkpad>
2014-04-16 19:30:30 +0200

    Updated linux starter script.

--- MixedRealityBot/bot_remotecontrol_linux_starter.sh
+++ MixedRealityBot/bot_remotecontrol_linux_starter.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-botPID=0
 
 function clean_up(){
-	if [$botPID -gt 0 ]; then
-		kill -s 9 $botPID
-		exit 0
-	fi
+	kill -s 9 $! > log
+	exit 0
 }
 
 trap clean_up SIGHUP SIGINT SIGTERM
 
 eval "$* &"
 
-i=1
-while [ $i -gt 0 ];
+while :
+do
+	echo "" > /dev/null
+done
+echo "end"
