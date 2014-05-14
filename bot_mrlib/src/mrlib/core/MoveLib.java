391aec356b7dd0ea871d7c5763b0f4c4164deedb
silps <silver.kuusik@gmail.com>
2014-05-14 14:47:44 +0200

    bot_mrlib/src/mrlib/core/MoveLib.java refactoring

--- bot_mrlib/src/mrlib/core/MoveLib.java
+++ bot_mrlib/src/mrlib/core/MoveLib.java
@@ -13,180 +13,173 @@ import essentials.communication.worlddata_server2008.ReferencePoint;
  *
  */
 public class MoveLib {
-	
 	/**
 	 * Angle to {@link ReferencePoint}, below that the agent doesn't turn.
 	 */
 	public static double moveAngleNoTurn = 10.0;
-	
+
 	/**
 	 * Angle to {@link ReferencePoint}, below that the agent should move and turn,
 	 * depending on the value of the angle to the {@link ReferencePoint}.
 	 * This value has to bee higher than {@link #moveAngleNoTurn}.
 	 */
 	public static double moveAngleTurnAndMove = 60.0;
-	
+
 	/**
 	 * Angle to {@link ReferencePoint}, below that the agent should turn with 25% speed only.
 	 */
 	public static double turnAngleSlowSpeed = 25.0;
-	
-	
 
 	/**
 	 * Running to a {@link ReferencePoint}.
 	 * @param aRefPoint	{@link ReferencePoint}
-	 * @return Action	{@link Action}
+	 * @return Action		{@link Action}
 	 */
-	public static Action runTo( ReferencePoint aRefPoint ){
+	public static Action runTo(ReferencePoint aRefPoint) {
 		return runTo( aRefPoint.getAngleToPoint() );
 	}
-	
+
 	/**
 	 * Running to a {@link FellowPlayer}.
 	 * @param aFellowPlayer	{@link FellowPlayer}
-	 * @return Action		{@link Action}
+	 * @return Action			{@link Action}
 	 */
-	public static Action runTo( FellowPlayer aFellowPlayer ) {
-		return runTo( aFellowPlayer.getAngleToPlayer() );
+	public static Action runTo(FellowPlayer aFellowPlayer) {
+		return runTo(aFellowPlayer.getAngleToPlayer());
 	}
-	
+
 	/**
 	 * Running to a {@link BallPosition}
 	 * @param aBallPosition	{@link BallPosition}
-	 * @return Action		{@link Action}
+	 * @return Action			{@link Action}
 	 */
-	public static Action runTo( BallPosition aBallPosition ) {
-		return runTo( aBallPosition.getAngleToBall() );
+	public static Action runTo(BallPosition aBallPosition) {
+		return runTo(aBallPosition.getAngleToBall());
 	}
-	
+
 	/**
 	 * Let the agent run into an specific direction.
 	 * @param vAngle	{@link Double} direction angle to run to in degree
 	 * @return Action	{@link Action}
 	 */
-	public static Action runTo( double vAngle ) {
-
+	public static Action runTo(double vAngle) {
 		// no turn fwd
-        if( vAngle >= -moveAngleNoTurn && vAngle <= moveAngleNoTurn ){            
-            return (Action) new Movement( 100, 100 );            
-        }
-        
-        // turn depending on angle fwd
-        if( vAngle > -moveAngleTurnAndMove && vAngle < -moveAngleNoTurn ){            
-            return (Action) new Movement( 100, 100 + (int) vAngle );            
-        }
-        
-        // turn depending on angle fwd
-        if( vAngle > moveAngleNoTurn && vAngle < moveAngleTurnAndMove ){            
-            return (Action) new Movement( 100 - (int) vAngle, 100 );            
-        }
-        
-        // turn on place left
-        if( vAngle >= -(180-moveAngleTurnAndMove) && vAngle <= -moveAngleTurnAndMove ){            
-            return (Action) new Movement( 100, -100 );            
-        }
-        
-        // turn on place right
-        if( vAngle >= moveAngleTurnAndMove && vAngle <= (180-moveAngleTurnAndMove) ){            
-            return (Action) new Movement( -100, 100 );            
-        }
-        
-        // turn depending on angle bwd
-        if( vAngle > -(180-moveAngleNoTurn) && vAngle < -(180-moveAngleTurnAndMove) ){            
-            return (Action) new Movement( -100, 100 + (int) vAngle );            
-        }
-        
-        // turn depending on angle bwd
-        if( vAngle > (180-moveAngleTurnAndMove) && vAngle < (180-moveAngleNoTurn) ){            
-            return (Action) new Movement( 100 - (int) vAngle, -100 );            
-        }
-        
-        // no turn bwd
-        if( ( vAngle >= (180-moveAngleNoTurn) && vAngle <= 180 )
-        		|| ( vAngle <= -(180-moveAngleNoTurn) && vAngle >= -180 ) ){            
-            return (Action) new Movement( -100, -100 );            
-        }
+		if (vAngle >= -moveAngleNoTurn && vAngle <= moveAngleNoTurn) {
+			return (Action) new Movement(100, 100);
+		}
 
-        return (Action) Movement.NO_MOVEMENT;
-    }
-	
+		// turn depending on angle fwd
+		if (vAngle > -moveAngleTurnAndMove && vAngle < -moveAngleNoTurn) {
+			return (Action) new Movement(100, 100 + (int) vAngle);
+		}
+
+		// turn depending on angle fwd
+		if (vAngle > moveAngleNoTurn && vAngle < moveAngleTurnAndMove) {
+			return (Action) new Movement(100 - (int) vAngle, 100);
+		}
+
+		// turn on place left
+		if (vAngle >= -(180 - moveAngleTurnAndMove) && vAngle <= - moveAngleTurnAndMove) {
+			return (Action) new Movement(100, -100);
+		}
+
+		// turn on place right
+		if (vAngle >= moveAngleTurnAndMove && vAngle <= (180 - moveAngleTurnAndMove)) {
+			return (Action) new Movement(-100, 100);
+		}
+
+		// turn depending on angle bwd
+		if (vAngle > -(180 - moveAngleNoTurn) && vAngle < -(180 - moveAngleTurnAndMove)) {
+			return (Action) new Movement(-100, 100 + (int) vAngle);
+		}
+
+		// turn depending on angle bwd
+		if (vAngle > (180 - moveAngleTurnAndMove) && vAngle < (180 - moveAngleNoTurn)) {
+			return (Action) new Movement(100 - (int) vAngle, -100);
+		}
+
+		// no turn bwd
+		if ((vAngle >= (180 - moveAngleNoTurn) && vAngle <= 180)
+				|| (vAngle <= - (180 - moveAngleNoTurn) && vAngle >= -180 )) {
+			return (Action) new Movement(-100, -100);
+		}
+
+		return (Action) Movement.NO_MOVEMENT;
+	}
+
 	/**
 	 * Turn to a {@link ReferencePoint}.
 	 * @param vRefPoint	{@link ReferencePoint}
 	 * @return			{@link Action}
 	 */
-	public static Action turnTo(ReferencePoint vRefPoint){
+	public static Action turnTo(ReferencePoint vRefPoint) {
 		return turnTo(vRefPoint.getAngleToPoint());
 	}
-	
+
 	/**
 	 * Turn to {@link FellowPlayer}
 	 * @param vFellowPlayer	{@link FellowPlayer}
 	 * @return				{@link Action}
 	 */
-	public static Action turnTo(FellowPlayer vFellowPlayer){
+	public static Action turnTo(FellowPlayer vFellowPlayer) {
 		return turnTo(vFellowPlayer.getAngleToPlayer());
 	}
-	
+
 	/**
 	 * Turn run to {@link BallPosition}.
 	 * @param vBallPos	{@link BallPosition}
 	 * @return			{@link Action}
 	 */
-	public static Action turnTo(BallPosition vBallPos){
+	public static Action turnTo(BallPosition vBallPos) {
 		return turnTo(vBallPos.getAngleToBall());
 	}
-	
+
 	/**
 	 * Turn towards an angle (no movement).
 	 * @param vAngle	{@link Double} angle to turn to.
-	 * @return			{@link Action}
+	 * @return		{@link Action}
 	 */
-	public static Action turnTo(double vAngle){
-		
+	public static Action turnTo(double vAngle) {
 		// no turn
-		if(vAngle < moveAngleNoTurn && vAngle > -moveAngleNoTurn
-				|| vAngle > (180-moveAngleNoTurn) && vAngle < -(180-moveAngleNoTurn)){
+		if (vAngle < moveAngleNoTurn && vAngle > -moveAngleNoTurn
+				|| vAngle > (180 - moveAngleNoTurn) && vAngle < -(180 - moveAngleNoTurn)) {
 			return (Action) Movement.NO_MOVEMENT;
 		}
-		
+
 		// 100% speed turn left
-		if(vAngle >= moveAngleTurnAndMove && vAngle <= (180-moveAngleTurnAndMove)){
-			return (Action) new Movement(-100,100);
+		if (vAngle >= moveAngleTurnAndMove && vAngle <= (180 - moveAngleTurnAndMove)) {
+			return (Action) new Movement(-100, 100);
 		}
-		
+
 		// 50% speed turn right
-		if(vAngle <= -moveAngleTurnAndMove && vAngle >= -(180-moveAngleTurnAndMove)){
-			return (Action) new Movement(100,-100);
+		if (vAngle <= -moveAngleTurnAndMove && vAngle >= -(180 - moveAngleTurnAndMove)) {
+			return (Action) new Movement(100, -100);
 		}
-		
+
 		// 50% speed turn left
-		if((vAngle >= turnAngleSlowSpeed && vAngle <= moveAngleTurnAndMove)
-				|| vAngle < -(180-moveAngleTurnAndMove) && vAngle > -(180-turnAngleSlowSpeed)){
-			return (Action) new Movement(-50,50);
+		if ((vAngle >= turnAngleSlowSpeed && vAngle <= moveAngleTurnAndMove)
+				|| vAngle < -(180 - moveAngleTurnAndMove) && vAngle > -(180 - turnAngleSlowSpeed)) {
+			return (Action) new Movement(-50, 50);
 		}
-		
+
 		// 50% speed turn right
-		if((vAngle < -turnAngleSlowSpeed && vAngle >= -moveAngleTurnAndMove)
-				|| vAngle > (180-moveAngleTurnAndMove) && vAngle < (180-turnAngleSlowSpeed)){
-			return (Action) new Movement(50,-50);
+		if ((vAngle < -turnAngleSlowSpeed && vAngle >= -moveAngleTurnAndMove)
+				|| vAngle > (180 - moveAngleTurnAndMove) && vAngle < (180 - turnAngleSlowSpeed)) {
+			return (Action) new Movement(50, -50);
 		}
-		
+
 		// 25% speed turn left
-		if((vAngle >= moveAngleNoTurn && vAngle <= turnAngleSlowSpeed)
-				|| vAngle < -(180-turnAngleSlowSpeed) && vAngle > -(180-moveAngleNoTurn)){
-			return (Action) new Movement(-25,25);
+		if ((vAngle >= moveAngleNoTurn && vAngle <= turnAngleSlowSpeed)
+				|| vAngle < -(180 - turnAngleSlowSpeed) && vAngle > -(180 - moveAngleNoTurn)) {
+			return (Action) new Movement(-25, 25);
 		}
-		
+
 		// 25% speed turn right
-		if((vAngle < -moveAngleNoTurn && vAngle >= -turnAngleSlowSpeed)
-				|| vAngle > (180-turnAngleSlowSpeed) && vAngle < (180-moveAngleNoTurn)){
+		if ((vAngle < -moveAngleNoTurn && vAngle >= -turnAngleSlowSpeed)
+				|| vAngle > (180 - turnAngleSlowSpeed) && vAngle < (180 - moveAngleNoTurn)) {
 			return (Action) new Movement(25,-25);
 		}
-		
+
 		return (Action) Movement.NO_MOVEMENT;
-		
 	}
-	
 }
