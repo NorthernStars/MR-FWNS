9ca61eab350dbc0b2cd089769c496fb4a42b1577
silps <silver.kuusik@gmail.com>
2014-05-14 13:41:58 +0200

    bot_mrlib/src/mrlib/core/PositionLib.java refactoring

--- bot_mrlib/src/mrlib/core/PositionLib.java
+++ bot_mrlib/src/mrlib/core/PositionLib.java
@@ -20,13 +20,12 @@ import essentials.communication.worlddata_server2008.ReferencePoint;
  *
  */
 public class PositionLib {
-
     /**
      * Gets {@link ReferencePoint} in middle of two other {@link ReferencePoint}.
      *
      * @since 0.9
-     * @param aRefPoint0 	First {@link ReferencePoint}
-     * @param aRefPoint1 	Second {@link ReferencePoint}
+     * @param aRefPoint0        First {@link ReferencePoint}
+     * @param aRefPoint1        Second {@link ReferencePoint}
      *
      * @return {@link ReferencePoint} in middle between {@code aRefPoint0} and {@code aRerfPoint1}.
      */
@@ -49,7 +48,7 @@ public class PositionLib {
             a = aRefPoint1.getDistanceToPoint();
         }
         double c = Math.sqrt(a*a + b*b - 2 * a * b * Math.cos(Math.toRadians(Math.abs(wa - wb))));
-        double sc = Math.sqrt((2 * (a*a + b*b)) - c*c ) / 2;
+        double sc = Math.sqrt((2 * (a*a + b*b)) - c*c) / 2;
         double gamma = Math.abs(Math.toDegrees(Math.acos((sc*sc + b*b - 0.25*c*c) / (2*sc*b))));
 
         if (wa < wb + 180) {
@@ -59,11 +58,11 @@ public class PositionLib {
         }
 
         if (gamma > 180) {
-        	gamma = gamma - 360;
+            gamma = gamma - 360;
         }
 
         if (gamma < -180) {
-        	gamma = gamma + 360;
+            gamma = gamma + 360;
         }
 
         return new ReferencePoint(sc, gamma, true);
@@ -71,14 +70,14 @@ public class PositionLib {
 
     /**
      * Returns the middle of enemies goal.
-     * @param aWorldData		{@link RawWorldData}
-     * @param aTeam 			Own {@link Teams} information
-     * @return ReferencePoint	{@link ReferencePoint} of middle of enemies goal.
+     * @param aWorldData                  {@link RawWorldData}
+     * @param aTeam                             Own {@link Teams} information
+     * @return ReferencePoint       {@link ReferencePoint} of middle of enemies goal.
      */
     public static ReferencePoint getMiddleOfGoal(RawWorldData aWorldData, Teams aTeam) {
         ReferencePoint vGoalTop = null;
         ReferencePoint vGoalBottom = null;
-        ReferencePoint rGoalMiddle = new ReferencePoint( 0.0 , 0.0, true );
+        ReferencePoint rGoalMiddle = new ReferencePoint(0.0 , 0.0, true);
 
         // get opponents goal
         if (aTeam == Teams.Yellow) {
@@ -95,14 +94,14 @@ public class PositionLib {
 
     /**
      * Returns the middle of own goal.
-     * @param aWorldData		{@link RawWorldData}
-     * @param aTeam 			Own {@link Teams} information
-     * @return ReferencePoint	{@link ReferencePoint} of middle of own goal.
+     * @param aWorldData                {@link RawWorldData}
+     * @param aTeam                           Own {@link Teams} information
+     * @return ReferencePoint     {@link ReferencePoint} of middle of own goal.
      */
     public static ReferencePoint getMiddleOfOwnGoal(RawWorldData aWorldData, Teams aTeam) {
-        ReferencePoint rOwnGoalMiddle = new ReferencePoint( 0.0 , 0.0, true );
         ReferencePoint vGoalTop = null;
         ReferencePoint vGoalBottom = null;
+        ReferencePoint rOwnGoalMiddle = new ReferencePoint(0.0 , 0.0, true);
 
         // get own goal
         if (aTeam == Teams.Blue) {
@@ -119,10 +118,10 @@ public class PositionLib {
 
     /**
      * Calculates if ball is in specific range around {@link ReferencePoint}.
-     * @param ballPos	{@link BallPosition}
-     * @param aRefPoint	{@link ReferencePoint}
-     * @param range		{@link Double} range
-     * @return			{@code true} if {@code ballPos} is in {@code range} around {@code aRefPoint}, {@code false} otherwise.
+     * @param ballPos        {@link BallPosition}
+     * @param aRefPoint    {@link ReferencePoint}
+     * @param range	            {@link Double} range
+     * @return                      {@code true} if {@code ballPos} is in {@code range} around {@code aRefPoint}, {@code false} otherwise.
      */
     public static boolean isBallInRangeOfRefPoint(BallPosition ballPos, ReferencePoint aRefPoint, double range) {
         double a, b, wa, wb;
@@ -148,9 +147,9 @@ public class PositionLib {
 
     /**
      * Calculates the distance between two {@link ReferencePoint}.
-     * @param aRefPoint0	{@link ReferencePoint}
-     * @param aRefPoint1	{@link ReferencePoint}
-     * @return				{@link Double} of distance between {@code aRefPoint0} and {@code aRefPoint1}.
+     * @param aRefPoint0        {@link ReferencePoint}
+     * @param aRefPoint1        {@link ReferencePoint}
+     * @return                             {@link Double} of distance between {@code aRefPoint0} and {@code aRefPoint1}.
      */
     public static double getDistanceBetweenTwoRefPoints(ReferencePoint aRefPoint0, ReferencePoint aRefPoint1) {
         double a, b, wa, wb;
@@ -165,8 +164,8 @@ public class PositionLib {
 
     /**
      * Get's the {@link ReferencePoint} farest away from ball.
-     * @param aWorldData		{@link RawWorldData}
-     * @return ReferencePoint	{@link ReferencePoint} farest away from ball.
+     * @param aWorldData                {@link RawWorldData}
+     * @return ReferencePoint     {@link ReferencePoint} farest away from ball.
      */
     public static ReferencePoint getFurthestPointAwayFromBall(RawWorldData aWorldData) {
         double vBestAngle = 0;
@@ -187,7 +186,7 @@ public class PositionLib {
 
         double vPointToBallAngle = 0;
         for (ReferencePoint vPoint : vEligiblePoints) {
-            vPointToBallAngle = Math.max(vPoint.getAngleToPoint(), aWorldData.getBallPosition().getAngleToBall() ) - Math.min( vPoint.getAngleToPoint(), aWorldData.getBallPosition().getAngleToBall());
+            vPointToBallAngle = Math.max(vPoint.getAngleToPoint(), aWorldData.getBallPosition().getAngleToBall()) - Math.min(vPoint.getAngleToPoint(), aWorldData.getBallPosition().getAngleToBall());
 
             if (vPointToBallAngle > 180) {
                 vPointToBallAngle = 360 - vPointToBallAngle;
@@ -204,9 +203,9 @@ public class PositionLib {
 
     /**
      * Calculates angle between two reference points
-     * @param aRefPoint0	{@link ReferencePoint}
-     * @param aRefPoint1	{@link ReferencePoint}
-     * @return				{@link Double} agnle between {@code aRefPoint0} and {@code aRefPoint1}.
+     * @param aRefPoint0        {@link ReferencePoint}
+     * @param aRefPoint1        {@link ReferencePoint}
+     * @return                             {@link Double} agnle between {@code aRefPoint0} and {@code aRefPoint1}.
      */
     public static double getAngleBetweenTwoReferencePoints(ReferencePoint aRefPoint0, ReferencePoint aRefPoint1) {
         double wa, wb;
@@ -228,11 +227,11 @@ public class PositionLib {
 
     /**
      * Calculates if bot is inside quadrilateral of four {@link ReferencePoint}s.
-     * @param aRefPoint0	{@link ReferencePoint}
-     * @param aRefPoint1	{@link ReferencePoint}
-     * @param aRefPoint2	{@link ReferencePoint}
-     * @param aRefPoint3	{@link ReferencePoint}
-     * @return				{@code true} if bot is inside quadrilateral {@code aRefPoint0} to {@code aRefPoint3}, {@code false} otherwise.
+     * @param aRefPoint0        {@link ReferencePoint}
+     * @param aRefPoint1        {@link ReferencePoint}
+     * @param aRefPoint2        {@link ReferencePoint}
+     * @param aRefPoint3        {@link ReferencePoint}
+     * @return                             {@code true} if bot is inside quadrilateral {@code aRefPoint0} to {@code aRefPoint3}, {@code false} otherwise.
      */
     public static boolean isBotInQuadrangle(ReferencePoint aRefPoint0, ReferencePoint aRefPoint1, ReferencePoint aRefPoint2, ReferencePoint aRefPoint3) {
         double angle1, angle2, angle3, angle4;
@@ -250,30 +249,30 @@ public class PositionLib {
 
     /**
      * Calculates if bot is inside soccer field.
-     * @param aWorldData	{@link RawWorldData}
-     * @return				{@code true} if bot is inside soccer field, {@code false} othwerwise.
+     * @param aWorldData        {@link RawWorldData}
+     * @return                             {@code true} if bot is inside soccer field, {@code false} othwerwise.
      */
     public static boolean isMeselfOutOfBounds(RawWorldData aWorldData) {
         return isBotInQuadrangle(aWorldData.getBlueFieldCornerTop(),
+            aWorldData.getYellowFieldCornerTop(),
             aWorldData.getBlueFieldCornerBottom(),
-            aWorldData.getYellowFieldCornerBottom(),
-            aWorldData.getYellowFieldCornerTop());
+            aWorldData.getYellowFieldCornerBottom());
     }
 
     /**
      * Calculates if ball is inside qaudrilateral of four {@link ReferencePoint}s.
      * ATTENTION: FUNCTION IS NOT WORKING! DON'T USE!
-     * @param aRefPoint0	{@link ReferencePoint}
-     * @param aRefPoint1	{@link ReferencePoint}
-     * @param aRefPoint2	{@link ReferencePoint}
-     * @param aRefPoint3	{@link ReferencePoint}
-     * @param ballPos		{@link BallPosition}
-     * @return				{@code true} if {@code ballPos} is inside quadrilateral {@code aRefPoint0} to {@code aRefPoint3}, {@code false} otherwise.
+     * @param aRefPoint0        {@link ReferencePoint}
+     * @param aRefPoint1        {@link ReferencePoint}
+     * @param aRefPoint2        {@link ReferencePoint}
+     * @param aRefPoint3        {@link ReferencePoint}
+     * @param ballPos              {@link BallPosition}
+     * @return                            {@code true} if {@code ballPos} is inside quadrilateral {@code aRefPoint0} to {@code aRefPoint3}, {@code false} otherwise.
      */
     public static boolean isBallInQuadrangle(ReferencePoint aRefPoint0, ReferencePoint aRefPoint1, ReferencePoint aRefPoint2, ReferencePoint aRefPoint3, BallPosition ballPos) {
         /*
          * TODO: Complete function to get if the Ball is in an area of 4 ReferencePoints
-         * */
+         */
         double smallestX = 0;
         @SuppressWarnings("unused")
         double secondsmallestX = 0;
@@ -298,14 +297,14 @@ public class PositionLib {
 
     /**
      * Calculate if agent is nearest team mate to a {@link ReferencePoint}.
-     * @param vWorldState	{@link RawWorldData}
-     * @param aRefPoint		{@link ReferencePoint}
-     * @param aSelf			{@link BotInformation}
-     * @return				{@code true} if bot is nearest team mate to {@code aRefPoint}.
+     * @param vWorldState    {@link RawWorldData}
+     * @param aRefPoint         {@link ReferencePoint}
+     * @param aSelf	                {@link BotInformation}
+     * @return                           {@code true} if bot is nearest team mate to {@code aRefPoint}.
      */
     public static boolean amINearestMateToPoint(RawWorldData vWorldState, ReferencePoint aRefPoint, BotInformation aSelf) {
         List<FellowPlayer> vTeamMates = vWorldState.getListOfTeamMates();
-        vTeamMates.add(new FellowPlayer(aSelf.getVtId(),"me", true,0,0,0) );
+        vTeamMates.add(new FellowPlayer(aSelf.getVtId(),"me", true, 0, 0, 0));
 
         FellowPlayer closest_player = null;
         for (FellowPlayer p: vTeamMates) {
@@ -316,7 +315,7 @@ public class PositionLib {
                 closest_player = p;
             }
         }
-
+        // confirm that it is realy you
         if (closest_player.getId() == aSelf.getVtId()) {
             return true;
         }
