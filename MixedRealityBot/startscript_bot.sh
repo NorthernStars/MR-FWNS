6645a9760ddf92d1942e0955d4f6d9f95bee9494
Hannes Eilers <mail@hanneseilers.de>
2016-10-13 19:02:16 +0200

    Updated classpaths and start scripts

--- MixedRealityBot/startscript_bot.sh
+++ MixedRealityBot/startscript_bot.sh
@@ -1,1 +1,1 @@
-java -cp "libraries/*;bot_mr.jar" core.Main -bn "MyBot" -tn "The Avangers" -t blau -ids 0 -s 127.0.0.1:3311 -aiarc "ais/example_ai.jar" -aicl "exampleai.brain.Striker"
+java -cp "bot_mr.jar:/libraries/*" core.Main -bn "MyBot" -tn "The Avangers" -t blau -ids 0 -s 127.0.0.1:3311 -aiarc "ais/example_ai.jar" -aicl "exampleai.brain.Striker"
