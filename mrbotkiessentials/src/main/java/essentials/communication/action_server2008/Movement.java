06ac50554f81d20583348cee612632b758b3fc92
sbruhse <sbruhse@sbruhse.de>
2017-03-29 15:42:47 +0200

    Revert "Implemented getter in class Movement"
    This reverts commit f55520858d4d23871c3bda61d270e4139d613ff4.

--- mrbotkiessentials/src/main/java/essentials/communication/action_server2008/Movement.java
+++ mrbotkiessentials/src/main/java/essentials/communication/action_server2008/Movement.java
@@ -31,12 +31,4 @@ public class Movement implements Action {
 		return "<command> <wheel_velocities> <right>" + mRightWheelVelocity + "</right> <left>" + mLeftWheelVelocity + "</left> </wheel_velocities> </command>";
 	}
 
-        public int getmRightWheelVelocity() {
-            return mRightWheelVelocity;
-        }
-
-        public int getmLeftWheelVelocity() {
-            return mLeftWheelVelocity;
-        }
-
 }
