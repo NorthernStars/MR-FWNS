c83cc42af15efaa4aad68a0b6d50723698c8281c
Lasse <git@lapete.de>
2017-07-27 11:39:18 +0200

    Fixed Merge error

--- mrbotkitools/src/test/java/mrlib/core/PlayersLibTests.java
+++ mrbotkitools/src/test/java/mrlib/core/PlayersLibTests.java
@@ -434,10 +434,6 @@ public class PlayersLibTests {
 
 		assertThat(testBool).isEqualTo(false);		
 		
-		testRefPoint.setAngleToPoint(179.0);
-		testBool = PlayersLib.isEnemyOnWayToRefPoint(worldModel, testRefPoint, 175.0);
-
-		assertThat(testBool).isEqualTo(true);
 		
 		
 	}
