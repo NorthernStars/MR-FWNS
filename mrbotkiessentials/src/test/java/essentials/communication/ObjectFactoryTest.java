fc0102339d83f9ab06c02c807e8bfd5231bb40da
sbruhse <sbruhse@sbruhse.de>
2017-08-28 13:15:50 +0200

    Added Test for ObjectFactory

---
+++ mrbotkiessentials/src/test/java/essentials/communication/ObjectFactoryTest.java
@@ -0,0 +1,31 @@
+package essentials.communication;
+
+import essentials.communication.worlddata_server2008.RawWorldData;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+public class ObjectFactoryTest
+{
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void testCreateRawWorldData()
+    {
+        ObjectFactory objectFactory = new ObjectFactory();
+
+        assertThat(objectFactory.createRawWorldData()).isNotInstanceOf(RawWorldData.class);
+    }
+
+}
