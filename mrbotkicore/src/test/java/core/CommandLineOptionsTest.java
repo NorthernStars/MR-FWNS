351e5e15d0a6efde0469cc7277ed5393e827ef9f
sbruhse <sbruhse@sbruhse.de>
2018-01-10 13:19:43 +0100

    Wrote tests for CommandLineOptions

--- mrbotkicore/src/test/java/core/CommandLineOptionsTest.java
+++ mrbotkicore/src/test/java/core/CommandLineOptionsTest.java
@@ -1,13 +1,22 @@
 package core;
 
+import essentials.core.BotInformation;
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.ParseException;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 public class CommandLineOptionsTest
 {
@@ -24,19 +33,52 @@ public class CommandLineOptionsTest
     }
 
     @Test
-    public void testParseOptions()
+    public void testParseCommandLineArgumentsWithNoExceptionsWithRemoteStart()
     {
-        CommandLineOptions commandLineOptions = new CommandLineOptions();
+        String[] args = {"-bn","3","-tn","Northern Stars","-t","blau","-ids","13","-s","localhost:3310","-aiarc","../mrbotkiexample/bin/exampleai/brain","-aicl","exampleai.brain.Striker","-aiarg","0","-rs"};
+        boolean result = CommandLineOptions.parseCommandLineArguments(args);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testParseCommandLineArgumentsWithNoExceptionsWithoutRemoteStart()
+    {
+        String[] args = {"-bn","3","-tn","Northern Stars","-t","blau","-ids","13","-s","localhost:3310","-aiarc","../mrbotkiexample/bin/exampleai/brain","-aicl","exampleai.brain.Striker","-aiarg","0"};
+        boolean result = CommandLineOptions.parseCommandLineArguments(args);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testParseCommandLineArgumentsWithNoExceptionsWithMissingArgument()
+    {
+        String[] args = {"-bn","3","-tn","Northern Stars","-t","blau","-ids","13","-s","-aiarc","../mrbotkiexample/bin/exampleai/brain","-aicl","exampleai.brain.Striker","-aiarg","0"};
+        boolean result = CommandLineOptions.parseCommandLineArguments(args);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testSetOptions()
+    {
+        String[] args = {"-bn","3","-tn","Northern Stars","-t","blau","-ids","13","-s","localhost:3310","-aiarc","../mrbotkiexample/bin/exampleai/brain","-aicl","exampleai.brain.Striker","-aiarg","0"};
+        CommandLineOptions.parseCommandLineArguments(args);
+        BotInformation botInformation = Core.getInstance().getBotinformation();
+
+        assertThat(botInformation.getAIArchive()).isEqualTo("../mrbotkiexample/bin/exampleai/brain");
+        assertThat(botInformation.getAIArgs()).isEqualTo("0");
+        assertThat(botInformation.getAIClassname()).isEqualTo("exampleai.brain.Striker");
         try
         {
-            CommandLine commandLine = commandLineOptions.parseOptions(new String[]{"-bn","3", "-tn",  "\"Northern Stars\""});
-
-            assertThat(commandLine).isInstanceOf(CommandLine.class);
+            assertThat(botInformation.getServerIP()).isEqualTo(InetAddress.getByName("localhost"));
         }
-        catch (ParseException e)
+        catch (UnknownHostException e)
         {
             e.printStackTrace();
         }
+        assertThat(botInformation.getServerPort()).isEqualTo(3310);
+        assertThat(botInformation.getRcId()).isEqualTo(13);
+        assertThat(botInformation.getTeam()).isEqualTo(BotInformation.Teams.Blue);
+        assertThat(botInformation.getTeamname()).isEqualTo("Northern Stars");
+        assertThat(botInformation.getBotname()).isEqualTo("3");
     }
 
 }
