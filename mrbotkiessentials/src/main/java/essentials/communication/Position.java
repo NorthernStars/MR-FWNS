544d1b90d7bf80acde2af24b44eb773665336d9b
WerthersEchte <arabali@gmx.de>
2018-02-20 21:47:11 +0100

    added orientation to position

--- mrbotkiessentials/src/main/java/essentials/communication/Position.java
+++ mrbotkiessentials/src/main/java/essentials/communication/Position.java
@@ -2,6 +2,7 @@ package essentials.communication;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Arrays;
 
 @XmlRootElement(name="Position")
 public class Position implements WorldData {
@@ -10,12 +11,11 @@ public class Position implements WorldData {
 	int[][] posi = {{-1, 0,-1},
 					{ 0, 1, 0},
 					{-1, 0,-1}};
-    @XmlElement(name="mXPositionInTile")
-	int mXPositionInTile;
-    @XmlElement(name="mYPositionInTile")
-	int mYPositionInTile;
-    @XmlElement(name="tileWidth")
-	int tileWidth;
-    @XmlElement(name="tileHeight")
-	int tileHeight;
+	@XmlElement(name="mXPositionInTile")
+	double mXPositionInTile;
+	@XmlElement(name="mYPositionInTile")
+	double mYPositionInTile;
+
+	@XmlElement(name="mOrientation")
+	double mOrientation;
 }
